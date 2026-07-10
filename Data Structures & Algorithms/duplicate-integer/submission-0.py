a9c3c66bc76e90fd35b1fca01b12e8e888abb945
Prithvi Singh Nathawat <66560711+PrithviSinghNathawat@users.noreply.github.com>
2026-07-10 23:28:52 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.py
@@ -0,0 +1,8 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        myset=set()
+        for x in nums:            
+            if x in myset:
+                return True
+            myset.add(x)
+        return False 
